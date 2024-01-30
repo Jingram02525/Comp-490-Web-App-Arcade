16a01a8719f0d7bda733afd657bc3f5884f0afc8
Luis Munoz <lmunozi181i@gmail.com>
2024-01-30 01:31:19 -0800

    Minor changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import RepHeader from './components/RepHeader';
+import RepMain from './components/RepMain';
+import RepFooter from './components/RepFooter';
+import {BrowserRouter as MyRouter, Route, Switch} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>hello world</code> 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RepHeader/>
+      <RepMain/>
+      <RepFooter/>
+    </>
   );
 }
 
