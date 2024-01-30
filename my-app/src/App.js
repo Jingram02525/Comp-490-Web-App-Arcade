fff1a68d6d45ae0c27d7f57d0150fe8bf34566eb
Luis Munoz <lmunozi181i@gmail.com>
2024-01-30 14:40:12 -0800

    Small Changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,13 +4,16 @@ import RepHeader from './components/RepHeader';
 import RepMain from './components/RepMain';
 import RepFooter from './components/RepFooter';
 import {BrowserRouter as MyRouter, Route, Switch} from 'react-router-dom';
-
+import { GbaProvider } from 'react-gbajs'
 
 function App() {
   return (
     <>
       <RepHeader/>
       <RepMain/>
+      <GbaProvider >
+
+      </GbaProvider>
       <RepFooter/>
     </>
   );
