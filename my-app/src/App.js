c1db98decd58fe8fa5fd8e4f18df7188cf527f3d
MannyAmza <mannyamza@gmail.com>
2024-02-13 14:56:05 -0800

    Removed App CSS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import RepHeader from './components/RepHeader';
 import RepMain from './components/RepMain';
 import RepFooter from './components/RepFooter';
