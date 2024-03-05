f24323703c8199434980c9f40f58f3027089a6e5
MannyAmza <mannyamza@gmail.com>
2024-03-05 15:13:25 -0800

    Ok home page looking better

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='text-center bg-blue-950 text-white mt-5 p-2.5'>
-            <p>&copy; 2023 RePlay | <a className='text-white no-underline hover:no-underline' href="terms.html">Terms and Conditions</a> | <a className='text-white no-underline hover:no-underline' href="privacy.html">Privacy Policy</a></p>
+        <footer>
+            <p>&copy; 2023 RePlay | <a href="terms.html">Terms and Conditions</a> | <a href="privacy.html">Privacy Policy</a></p>
         </footer>
     );
 }
--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 
 const Navbar = () => {
     return(
-    <header className='w-full z-10 bg-blue-950 opacity-90' text-white p-5>
+      <header className='bg-[#1E0B41] text-white p-5'>
       <nav className='flex justify-between items-center'>
-        <div className='text-2xl no-underline text-white font-bold'>
-          <a href="index.html" >RePlay</a>
+        <div>
+          <a href="index.html" className='text-2xl no-underline text-white font-bold'>RePlay</a>
         </div>
-        <div className='flex items-center justify-center mt-2.5;'>
-          <input type="text" placeholder="Search" className='text-base p-2.5 rounded-[5px] border-[none];'/>
-          <button type="submit" className='bg-[#555] text-white cursor-pointer p-2.5 rounded-[5px] border-[none];'>Search</button>
+        <div className='flex items-center justify-center mt-2 no-underline'>
+          <input type="text" placeholder="ROMs, Games, etc,..." 
+          className='p-2.5 rounded text-base bg-[#250E4F] text-white mr-2.5 no-underline border-[#250E4F]'/>
+          <button type="submit"className='bg-[#250E4F] text-orange-400 cursor-pointer font-[bold] 
+          no-underline p-2.5 rounded-[5px] border-solid border-[#250E4F] hover:bg-orange-400 
+          hover:text-[white] hover:font-[bold] hover:no-underline hover:transition-[color] 
+          hover:duration-[0.3s]'>Search</button>
         </div>
-        <ul className='flex list-style: none;'>
-          <li className='mr-8;'><a href="roms.html" className='no-underline text-white font-bold transition-[color] duration-[0.3s] hover:text-[#ddd] active:text-[#ddd] visited:underline;'>ROMs</a></li>
-          <li className='mr-5;'><a href="games.html" className='no-underline text-white font-bold transition-[color] duration-[0.3s] hover:text-[#ddd] active:text-[#ddd] visited:underline;'>Games</a></li>
-          <li className='mr-5;'><a href="login.html" className='no-underline text-white font-bold transition-[color] duration-[0.3s] hover:text-[#ddd] active:text-[#ddd] visited:underline;'>Login</a></li>
-          <li className='mr-5;'><a href="register.html" className='no-underline text-white font-bold transition-[color] duration-[0.3s] hover:text-[#ddd] active:text-[#ddd] visited:underline;'>Sign Up</a></li>
-        </ul>
+        <div className="nav-links">
+          <a href="roms.html" className="button roms">ROMs</a>
+          <a href="games.html" className="button games"> Games</a>
+          <a href="login.html" className="button login">Login</a>
+          <a href="register.html" className="button sign-up">Sign Up</a>
+        </div>
       </nav>
     </header>
   );
