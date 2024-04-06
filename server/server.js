71ecaa866d5a4d0f5aed325c5e5a763f6a0ddc14
Luis Munoz <lmunozi181i@gmail.com>
2024-04-06 12:52:30 -0700

    Fixed alot bugs, and now server is working

--- server/server.js
+++ server/server.js
@@ -1,48 +1,53 @@
 const express = require('express')
-const mysql = require('mysql2')
 const cors = require('cors')
-
 const app = express()
+const PORT = 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors())
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",    
-    password: 'Lambda-3000',
-    database: "com440",
+app.get('/api', (req, res) => {
+    res.json({message:"Hello world!"});
 })
 
-db.connect((err) => {
-    if(err) {
-        console.error('Error connecting to MYSQL:', err)
-    } else {
-        console.log('Connected to MYSQL database:')
-    }
-})
+// const db = mysql.createConnection({
+//        host: "localhost",
+//         port: "3306",
+//         user: "root",
+//         password: '1234',
+//         database: "webgame",
+// })
 
-app.get('/', (req, res) => {
-    return res.json("From Backend Side");
-})
+// db.connect((err) => {
+//     if(err) {
+//         console.error('Error connecting to MYSQL:', err)
+//     } else {
+//         console.log('Connected to MYSQL database:')
+//     }
+// })
 
-app.get('/users', (req, res) => {
-    const sql = "SELECT * FROM com440.item";
-    db.query(sql, (err, data) => {
-        if(err) return res.json(err);
-        return res.json(data);
-    })
-})
 
-// const db = mysql.createPool({
-//     host: "localhost",
-//     port: "3306",
-//     user: "root",
-//     password: '1234',
-//     database: "webgame",
 
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// app.post('/signup', (req, res) => {
+//     const { firstName } = req.body;
+
+//     console.log(req.body);
+
+//    //  Insert form data into the database
+//     db.query('INSERT INTO test (first) VALUES (?)',
+//        [firstName],
+//         (error, results) => {
+//             if (error) {
+//                 console.error('Error inserting data:', error);
+//                 return res.status(500).json({ error: 'An error occurred while processing your request.' });
+//             }
+//             res.status(200).json({ message: 'Form submitted successfully.' });
+//         });
 // });
-
-
-
-app.listen(8081, () =>{
-    console.log("listening");
-})
