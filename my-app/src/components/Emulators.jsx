6eda0f426b53b57b3c7e969fc4cf7495204f8cb3
ICUP69 <113947332+ICUP69@users.noreply.github.com>
2024-05-09 11:14:29 -0700

    emu verc and details

--- my-app/src/components/Emulators.jsx
+++ my-app/src/components/Emulators.jsx
@@ -1,6 +1,7 @@
 
 import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
+import "../styles/emulators.css";
 
 const Emulators = () => {
   const [username, setUsername] = useState("");
