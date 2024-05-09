fad029a291427feda5365f68bb704151f45a7568
Luis Munoz <lmunozi181i@gmail.com>
2024-05-09 13:50:32 -0700

    fdsa

--- my-app/src/components/Emulators.jsx
+++ my-app/src/components/Emulators.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
-import "../styles/Emulators.css";
+import "../styles/emulators.css";
 
 const Emulators = () => {
   const [username, setUsername] = useState("");
